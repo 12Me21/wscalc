cd03b7adca54e426476de94b5f439365703ea063
12Me21 <12Me21.mc@gmail.com>
2021-01-14 15:35:55 -0500

    e

--- main.lua
+++ main.lua
@@ -1,81 +1,56 @@
 local INFIX = {
 	["+"]=function(a,b) return a+b end,
 	["-"]=function(a,b) return a-b end,
+	["*"]=function(a,b) return a*b end,
+	["/"]=function(a,b) return a/b end,
 }
 local PREFIX = {
 	["-"]=function(a) return -a end,
 }
-
-function readExpr(str)
-	local acc, str = readValue(str)
-	::found::
-	if str:sub(1,1)==")" then
-		return acc, str:sub(2)
+function search(str, map)
+	for k,v in pairs(map) do
+		if str:sub(1,#k)==k then return k,v	end
 	end
-	if str=="" then
-		return acc, str
-	end
-	for op,f in pairs(INFIX) do
-		if str:sub(1,#op)==op then
-			local v
-			v, str = readValue(str:sub(#op+1))
-			acc = f(acc, v)
-			goto found
-		end
-	end
-	return nil, str
 end
 
-function readValue(str)
+function readExpr(str, depth)
+	local acc, str = readValue(str, depth)
+
+	::again::
+	if str:sub(1,1)==" " then -- end group
+		str = str:sub(2)
+		if depth>0 then return acc, str end
+	end
+	if str=="" then return acc, str end -- end
+
+	local op, f = search(str, INFIX)
+	if op then
+		local v
+		v, str = readValue(str:sub(#op+1), depth)
+		acc = f(acc, v)
+		goto again
+	end
+	error("syntax error: "..str)
+end
+
+function readValue(str, depth)
+
 	local num = str:match("^%d+")
 	if num then
 		return tonumber(num), str:sub(#num+1)
 	end
-	if str:sub(1,1) == "(" then
-		return readExpr(str:sub(2))
+
+	if str:sub(1,1) == " " then -- start group
+		return readExpr(str:sub(2), depth+1)
 	end
-	for op,f in pairs(PREFIX) do
-		if str:sub(1,#op)==op then
-			local v
-			v,str = readValue(str:sub(#op+1))
-			return f(v), str
-		end
+
+	local op, f = search(str, PREFIX)
+	if op then
+		local v
+		v, str = readValue(str:sub(#op+1), depth)
+		return f(v), str
 	end
-	return nil
+	error("syntax error: "..str)
 end
 
-function readValue2(str)
-	for op,f in pairs(PREFIX) do
-		if str:sub(1,#op)==op then
-			local v, str2 = readValue(str:sub(#op+1))
-			return f(v), str2
-		end
-	end
-	local num = str:match("%d+")
-	if num then
-		str=str:sub(#num+1)
-		local acc = tonumber(num)
-		::found::
-		for op,f in pairs(INFIX) do
-			if str:sub(1,#op)==op then
-				local v
-				v, str = readValue(str:sub(#op+1))
-				acc = f(acc, v)
-				goto found
-			end
-		end
-		if str:sub(1,1) == ")" then
-			return acc, str:sub(2)
-		end
-		if str=="" then
-			return acc,str
-		end
-		return nil, str
-	end
-	if str:sub(1,1)=="(" then
-		return readValue(str:sub(2))
-	end
-	return nil, str
-end
-
-print(readExpr(arg[1]))
+print(readExpr(arg[1], 0))
