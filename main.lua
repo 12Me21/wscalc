f0205722837e645e565039d9d0e93345d5fec04e
12Me21 <12Me21.mc@gmail.com>
2021-01-14 17:26:36 -0500

    e

--- main.lua
+++ main.lua
@@ -21,11 +21,13 @@ function search(str, map)
 	end
 end
 
+-- return: value, string
 function readExpr(str, depth)
 	local acc, str = readValue(str, depth)
 	return readAfter(str, depth, acc)
 end
 
+-- return: value, string
 function readAfter(str, depth, acc)
 	if str:sub(1,1)==" " then -- end group
 		str = str:sub(2)
@@ -42,13 +44,13 @@ function readAfter(str, depth, acc)
 	error("syntax error: "..str)
 end
 
+-- return: value, string
 function readValue(str, depth)
-
-	local num = str:match("^%d*.?%d+")
+	
+	local num = str:match("^%d*%.?%d+")
 	if num then
 		return tonumber(num), str:sub(#num+1)
 	end
-
 	if str:sub(1,1) == " " then -- start group
 		return readExpr(str:sub(2), depth+1)
 	end
@@ -62,4 +64,18 @@ function readValue(str, depth)
 	error("syntax error: "..str)
 end
 
-print(readExpr(arg[1], 0))
+function eval(exp, acc)
+	local status, res = pcall(readAfter, exp, 0, acc)
+	if status then return res end
+	status, res = pcall(readExpr, exp, 0)
+	if status then return res end
+	print(res)
+	return acc
+end
+
+local acc = 0
+repeat
+	local line = io.read()
+	acc = eval(line, acc)
+	print(">", acc)
+until nil
